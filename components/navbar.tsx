34e7bc2d9b8ca50c8588bd95361d9df7a573eda7
kojimakojima <koko1217.kojima@gmail.com>
2023-12-06 14:03:21 +0100

    initial commit

---
+++ components/navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ModeToggle } from "./mode-toggle";
+import Link from "next/link";
+import { Cat } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center p-2 gap-2">
+      <Link href={"/"}>
+        <Cat size={28} />
+      </Link>
+      <ModeToggle />
+    </div>
+  );
+}
