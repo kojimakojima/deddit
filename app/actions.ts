43304455ca593979e33f8b992b9ad01c36bf6ceb
kojimakojima <koko1217.kojima@gmail.com>
2023-12-07 22:14:05 +0100

    "/"

--- app/actions.ts
+++ app/actions.ts
@@ -21,7 +21,7 @@ export async function createThread(formData: FormData) {
       },
     });
 
-    revalidatePath("");
+    revalidatePath("/");
     return "success";
   } catch (error) {
     return "error";
