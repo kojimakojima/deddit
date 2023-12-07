9cdaf79aa05795391b609eb1a479ad4534ebb3ac
kojimakojima <koko1217.kojima@gmail.com>
2023-12-07 21:29:14 +0100

    revalidatePath "page"

--- app/actions.ts
+++ app/actions.ts
@@ -21,7 +21,7 @@ export async function createThread(formData: FormData) {
       },
     });
 
-    revalidatePath("/");
+    revalidatePath("/", "page");
     return "success";
   } catch (error) {
     return "error";
